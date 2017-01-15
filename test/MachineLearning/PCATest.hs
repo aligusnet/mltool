548cd53d6fd100fa62ca9efaee436071e060df05
Alexander Ignatyev <alexander.ignatyev@yahoo.com>
2017-01-15 14:07:49 +0000

    added PCA unit tests

--- test/MachineLearning/PCATest.hs
+++ test/MachineLearning/PCATest.hs
@@ -8,7 +8,7 @@ where
 import Test.Framework (testGroup)
 import Test.Framework.Providers.HUnit
 import Test.HUnit
-import Test.HUnit.Plus
+import Test.HUnit.Approx
 
 import qualified Numeric.LinearAlgebra as LA
 
@@ -19,18 +19,19 @@ getDimReducerSmokeTest =
       nExamples = 7
       m = LA.matrix nFeatures [1 .. fromIntegral $ nFeatures*nExamples]
       m10 = m * 10
-      (reduceDims, _, mReduced) = getDimReducer m 2
+      (reduceDims, retainedVariance, mReduced) = getDimReducer m 2
       m10Reduced = reduceDims m10
   in do
     assertEqual "dimension equality (getDimReducer)" (LA.cols mReduced) 2
     assertEqual "dimension equality (reduceDims)" (LA.cols m10Reduced) 2
+    assertApproxEqual "retained variance" 1e-10 1 retainedVariance
 
-getDimReducer_rvSmokeTest =
+getDimReducer_rvSmokeTest rv=
   let nFeatures = 4
       nExamples = 7
       m = LA.matrix nFeatures [1 .. fromIntegral $ nFeatures*nExamples]
       m10 = m * 10
-      (reduceDims, k, mReduced) = getDimReducer_rv m 0.5
+      (reduceDims, k, mReduced) = getDimReducer_rv m rv
       m10Reduced = reduceDims m10
   in do
     assertEqual "dimension equality (getDimReducer_rv)" (LA.cols mReduced) k
@@ -39,6 +40,9 @@ getDimReducer_rvSmokeTest =
 
 tests = [ testGroup "smoke test" [
             testCase "getDimReducer" getDimReducerSmokeTest
-            , testCase "getDimReducer_rv" getDimReducer_rvSmokeTest
+            , testCase "getDimReducer_rv, rv = 0" $ getDimReducer_rvSmokeTest 0
+            , testCase "getDimReducer_rv, rv = 0.5" $ getDimReducer_rvSmokeTest 0.5
+            , testCase "getDimReducer_rv, rv = 1" $ getDimReducer_rvSmokeTest 1
+            , testCase "getDimReducer_rv, rv = 2" $ getDimReducer_rvSmokeTest 2
             ]
         ]
