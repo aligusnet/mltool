c7b6de610edcd2f07dbc183b696f7d0e936fb968
Alexander Ignatyev <ignatyev.alexander@gmail.com>
2017-03-04 00:14:26 +0000

    splited theta to b and w in nn

--- samples/neural_networks/Main.hs
+++ samples/neural_networks/Main.hs
@@ -22,16 +22,14 @@ main = do
   -- Step 3. Initialize theta with randon values.
       initTheta = NN.initializeTheta 5191711 nnt
 
-      x1 = ML.addBiasDimension x
-
       lambda = 5 / (fromIntegral $ LA.rows x)
 
   -- Step 4. Learn the Neural Network.
-  (thetaNN, optPath) <- TP.learnWithProgressBar (Opt.minimize (Opt.BFGS2 0.03 0.7) model 1e-7 5 lambda x1 y) initTheta 20
+  (thetaNN, optPath) <- TP.learnWithProgressBar (Opt.minimize (Opt.BFGS2 0.03 0.7) model 1e-7 5 lambda x y) initTheta 20
 
   -- Step 5. Making predictions and checking accuracy on training and test sets.
-  let accuracyTrain = NN.calcAccuracy y (NN.hypothesis model x1 thetaNN)
-      accuracyTest = NN.calcAccuracy yTest (NN.hypothesis model (ML.addBiasDimension xTest) thetaNN)
+  let accuracyTrain = NN.calcAccuracy y (NN.hypothesis model x thetaNN)
+      accuracyTest = NN.calcAccuracy yTest (NN.hypothesis model xTest thetaNN)
 
   -- Step 6. Printing results.
 
--- test/MachineLearning/NeuralNetworkTest.hs
+++ test/MachineLearning/NeuralNetworkTest.hs
@@ -22,7 +22,6 @@ import MachineLearning.NeuralNetwork
 
 (x, y) = ML.splitToXY dataset2
 
-x1 = ML.addBiasDimension x
 nnt = makeTopology (LA.cols x) 2 [10]
 model = NeuralNetwork nnt
 
@@ -30,7 +29,7 @@ gradientCheckingEps = 0.1
 
 checkGradientTest lambda = do
   let thetas = initializeTheta 1511197 nnt
-      diffs = take 5 $ map (\e -> Opt.checkGradient model lambda x1 y thetas e) [0.005, 0.0051 ..]
+      diffs = take 5 $ map (\e -> Opt.checkGradient model lambda x y thetas e) [0.005, 0.0051 ..]
       diff = minimum $ filter (not . isNaN) diffs
   assertApproxEqual (show thetas) gradientCheckingEps 0 diff
 
@@ -51,10 +50,10 @@ yExpected = LA.vector [1, 1, 0, 0, 1]
 
 learnTest minMethod =
   let lambda = 0.5 / (fromIntegral $ LA.rows x)
-      x1 = ML.addBiasDimension $ ML.mapFeatures 2 x
-      nnt = makeTopology ((LA.cols x1) - 1) 2 [10]
+      x1 = ML.mapFeatures 2 x
+      nnt = makeTopology (LA.cols x1) 2 [10]
       model = NeuralNetwork nnt
-      xPredict1 = ML.addBiasDimension $ ML.mapFeatures 2 xPredict
+      xPredict1 = ML.mapFeatures 2 xPredict
       initTheta = initializeTheta 5191711 nnt
       (theta, optPath) = Opt.minimize minMethod model 1e-7 100 lambda x1 y initTheta
       yPredicted = hypothesis model xPredict1 theta
